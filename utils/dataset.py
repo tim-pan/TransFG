eeefc8793b29a9d1c8df406a8997490ad0535ded
tim-pan <65237732+tim-pan@users.noreply.github.com>
2021-12-10 15:05:03 +0800

    Add files via upload

--- utils/dataset.py
+++ utils/dataset.py
@@ -567,64 +567,55 @@ class INat2017(VisionDataset):
             if not check_integrity(os.path.join(self.root, filename), md5):
                 raise RuntimeError("File not found or corrupted.")
 
+
 class fish():
-    def __init__(self, root, is_train=True, data_len=None, transform=None):
-        self.root = root
+    def __init__(self, root, is_train=True, transform=None):
+        self.root = root#/content/.../the-nature-conservancy-fisheries-monitoring/
         self.is_train = is_train
         self.transform = transform
-#         img_txt_file = open(os.path.join(self.root, 'images.txt'))
-#         label_txt_file = open(os.path.join(self.root, 'image_class_labels.txt'))
-#         train_val_file = open(os.path.join(self.root, 'train_test_split.txt'))
-        img_name_list = []
-        for line in img_txt_file:
-            img_name_list.append(line[:-1].split(' ')[-1])
-        label_list = []
-        for line in label_txt_file:
-            label_list.append(int(line[:-1].split(' ')[-1]) - 1)
-        train_test_list = []
-        for line in train_val_file:
-            train_test_list.append(int(line[:-1].split(' ')[-1]))
-        train_file_list = [x for i, x in zip(train_test_list, img_name_list) if i]
-        test_file_list = [x for i, x in zip(train_test_list, img_name_list) if not i]
+        self.classes = ['ALB', 'BET', 'DOL', 'LAG', 'Nof', 'OTHER', 'SHARK', 'YFT']
+        
+        self.img_list = []
+        self.img_name_list = []
+        self.img_label_list = []
+
         if self.is_train:
-            self.train_img = [scipy.misc.imread(os.path.join(self.root, 'images', train_file)) for train_file in
-                              train_file_list[:data_len]]
-            self.train_label = [x for i, x in zip(train_test_list, label_list) if i][:data_len]
-            self.train_imgname = [x for x in train_file_list[:data_len]]
+            for cla in self.self.classes:
+                for file_name in os.listdir(os.path.join(self.root, 'train', cla)):
+                    train_img = scipy.misc.imread(os.path.join(self.root, 'train', cla, file_name))
+                    self.img_list.append(train_img)
+                    self.img_name_list.append(file_name)
+                    self.img_label_list.append(cla)
+
         if not self.is_train:
-            self.test_img = [scipy.misc.imread(os.path.join(self.root, 'images', test_file)) for test_file in
-                             test_file_list[:data_len]]
-            self.test_label = [x for i, x in zip(train_test_list, label_list) if not i][:data_len]
-            self.test_imgname = [x for x in test_file_list[:data_len]]
+            for file_name in os.listdir(os.path.join(self.root, 'test_stg1')):
+                train_img = scipy.misc.imread(os.path.join(self.root, 'train', file_name))
+                self.img_list.append(train_img)
+                self.img_name_list.append(file_name)
+                self.img_label_list.append(cla)
+
+
+
     def __getitem__(self, index):
         if self.is_train:
-            img, target, imgname = self.train_img[index], self.train_label[index], self.train_imgname[index]
+            img, target, imgname = self.img_list[index], self.img_label_list[index], self.img_name_list[index]
             if len(img.shape) == 2:
                 img = np.stack([img] * 3, 2)
             img = Image.fromarray(img, mode='RGB')
             if self.transform is not None:
                 img = self.transform(img)
         else:
-            img, target, imgname = self.test_img[index], self.test_label[index], self.test_imgname[index]
+            img, target, imgname = self.img_list[index], self.img_label_list[index], self.img_name_list[index]
             if len(img.shape) == 2:
                 img = np.stack([img] * 3, 2)
             img = Image.fromarray(img, mode='RGB')
             if self.transform is not None:
                 img = self.transform(img)
 
-        return img, target
+        return img, target, imgname
 
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
+    def __len__(self):
+        return img_label_list
                 
                 
                 
